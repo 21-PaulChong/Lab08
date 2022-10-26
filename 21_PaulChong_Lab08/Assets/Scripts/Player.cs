a704950280b865406fca36adaae855fb59fcaf8a
PoggerPaul <114891770+21-PaulChong@users.noreply.github.com>
2022-10-26 10:56:22 +0800

    Update Complete
    Finished All Unity Build Requirements
    Added Git.Ignore

---
+++ 21_PaulChong_Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Text scoretext;
+    public static int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+        scoretext.text = "Score: " + score;
+      
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+
+}
